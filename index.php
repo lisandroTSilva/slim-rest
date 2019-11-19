5579bffe19d252bdab28a35b3446cddc742826f0
Lisandro Silva <lisandro.t.silva@gmail.com>
2019-11-19 16:42:03 -0300

    Correção no uso da classe Aluno.

--- index.php
+++ index.php
@@ -4,26 +4,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
-use Rest\Alunos;
 use Slim\Routing\RouteCollectorProxy;
+use Rest\Alunos;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
 
+/* Middleware que fixa o content-type como json */
 $app->add(function (Request $request, RequestHandler $handler) {
     return $handler
         ->handle($request)
         ->withHeader('Content-type', 'application/json');
 });
 
-//Alunos::configRoutes($app);
-$app->group('/alunos', function (RouteCollectorProxy $group) {
-    $group->get('/', function (Request $request, Response $response, $args) {
-        $data = array('name' => 'Bob', 'age' => 40);
-        $payload = json_encode($data);
-        return $response->getBody()->write($payload);
-    });
-});
-
+Alunos::configRoutes($app);
 $app->run();
--- rest/Alunos.php
+++ rest/Alunos.php
@@ -7,30 +7,53 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
-class Alunos
+final class Alunos
 {
     static public function configRoutes(App $app)
     {
         $app->group('/alunos', function (RouteCollectorProxy $group) {
-            $group->get('/', \HomeController::class . ':index');
-            //$group->get('/{id:[0-9]+}', \HomeController::class . ':get');
+            $group->get('', Alunos::class . ":index");
+            $group->get('/{id:[0-9]+}', Alunos::class . ":getItem");
+            $group->post('', Alunos::class . ":create");
+            $group->put('/{id:[0-9]+}', Alunos::class . ":update");
+            $group->delete('/{id:[0-9]+}', Alunos::class . ":delete");
         });
-        // ->add(new GroupMiddleware());
     }
 
     public static function index(Request $request, Response $response, $args)
     {
-        $data = array('name' => 'Bob', 'age' => 40);
-        $payload = json_encode($data);
-        return $response->getBody()->write($payload);
-        // return $response
-        //     ->withHeader('Content-Type', 'application/json');
+        $response
+            ->getBody()
+            ->write(json_encode(self::getAlunos()));
+        return $response;
     }
 
-    public static function get(Request $request, Response $response, $args)
+    public static function getItem(Request $request, Response $response, $args)
     {
-        $data = array('name' => 'Bob', 'age' => 40);
-        $payload = json_encode($data);
-        return $response->getBody()->write($payload);
+        $data = array_filter(self::getAlunos(), function ($item) use ($args) {
+            return $item['id'] == $args['id'];
+        });
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    }
+
+    public static function create(Request $request, Response $response, $args)
+    { }
+
+    public static function update(Request $request, Response $response, $args)
+    { }
+
+    public static function delete(Request $request, Response $response, $args)
+    { }
+
+    private static function getAlunos()
+    {
+        return [
+            ['id' => 1, 'name' => 'Lisandro Silva'],
+            ['id' => 2, 'name' => 'Diogo Paludo'],
+            ['id' => 3, 'name' => 'Douglas Silveira'],
+            ['id' => 4, 'name' => 'Vanderson'],
+            ['id' => 5, 'name' => 'Fábio']
+        ];
     }
 }
